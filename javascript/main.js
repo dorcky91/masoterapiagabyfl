45644a90a76cdee4b9a1aa25cd3bd50d6db26b2d
dorcky91 <orlandoappolon1@gmail.com>
2023-06-25 00:39:33 -0600

    Mi Primer Commit

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+var btnMenu = document.getElementById("navegacion-movil");
+var btnCerrar = document.getElementById("cerrar-navegacion")
+
+btnMenu.addEventListener("click", function(){
+    var menu = document.getElementById("navegacion")
+    menu.classList.toggle("activo")
+})
+btnCerrar.addEventListener("click", function(){
+    var menu = document.getElementById("navegacion")
+    menu.classList.toggle("activo")
+})
