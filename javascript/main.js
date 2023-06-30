3c5e55c82ddd62c4c6708e1c30d523a90487612b
dorcky91 <orlandoappolon1@gmail.com>
2023-06-29 23:38:58 -0600

    Funcionalidad Del Menu Móvil

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,11 @@
 var btnMenu = document.getElementById("navegacion-movil");
-var btnCerrar = document.getElementById("cerrar-navegacion")
+var btnCerrar = document.getElementById("cerrar-navegacion");
 
-btnMenu.addEventListener("click", function(){
-    var menu = document.getElementById("navegacion")
-    menu.classList.toggle("activo")
-})
-btnCerrar.addEventListener("click", function(){
-    var menu = document.getElementById("navegacion")
-    menu.classList.toggle("activo")
-})
+btnMenu.addEventListener("click", function () {
+  var menu = document.getElementById("navegacion");
+  menu.classList.toggle("activo");
+});
+btnCerrar.addEventListener("click", function () {
+  var menu = document.getElementById("navegacion");
+  menu.classList.toggle("activo");
+});
