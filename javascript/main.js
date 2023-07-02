1bf1423c031a33936edaa3b0dabadebc730e0bcf
dorcky91 <orlandoappolon1@gmail.com>
2023-07-01 23:01:54 -0600

    Agregando Animación

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,19 @@ btnCerrar.addEventListener("click", function () {
   var menu = document.getElementById("navegacion");
   menu.classList.toggle("activo");
 });
+
+let animacionImagen = document.querySelectorAll(".animacion");
+
+let observador = new IntersectionObserver((entradas) =>
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("visible");
+    } else {
+      entrada.target.classList.remove("visible");
+    }
+  })
+);
+
+// animacionImagen.forEach((element) => {
+//   observador.observe(element);
+// });
